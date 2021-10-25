a467e54c8c17a049b17deb75f37f99badbb47236
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-25 17:02:47 +0800

    Added conditions

--- User_Input.py
+++ User_Input.py
@@ -1,5 +1,13 @@
-while True:
-    usr_name = input("Enter your name: ")
-    if usr_name != "":
-        print(f"Welcome to the shop, {usr_name}!")
-        break
+def usr_inpt():
+    query = 0
+    while True:
+        usr_name = input("Enter your name: ")
+        query += 1
+        if usr_name != "":
+            print(f"Welcome to the shop, {usr_name}!")
+            break
+        elif query == 5:
+            print("Please try again later.")
+            return None
+
+usr_inpt()
